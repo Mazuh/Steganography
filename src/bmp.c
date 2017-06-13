2dd11498e0e054e537de621c4ebd772fe81cb042
Mazuh <marcell-mz@hotmail.com>
2017-06-13 14:50:00 -0300

    #4 Read image header

--- src/bmp.c
+++ src/bmp.c
@@ -11,6 +11,9 @@
 const char BMP_DEFAULT_ASSET_FILENAME[] = "./default_assets/imd.bmp";
 const char BMP_DEFAULT_ENCODED_FILENAME[] = "./encoded_images/suspicious_bitmap.bmp";
 
+#define SIZE_INT 4
+#define SIZE_SHORT 2
+
 BMP _image;
 
 void _bmp_read_from_file(const char *filename, int secret){
@@ -20,20 +23,58 @@ void _bmp_read_from_file(const char *filename, int secret){
     printf("File: %s\n", filename);
 
     // scanning file header
-    //unsigned short type = 19778;
     char type[3];
     unsigned int size;
     unsigned int reserved;
     unsigned int offset;
-    
+
     type[0] = fgetc(img_file);
     type[1] = fgetc(img_file);
     type[2] = '\0';
-    fread(&size, 4, 1, img_file);
-    fread(&reserved, 4, 1, img_file);
-    fread(&offset, 4, 1, img_file);
+    fread(&size, SIZE_INT, 1, img_file);
+    fread(&reserved, SIZE_INT, 1, img_file);
+    fread(&offset, SIZE_INT, 1, img_file);
 
     printf("Type: %s, Size: %u, Reserved: %u, Offset: %u\n", type, size, reserved, offset);
+
+    // scanning image header
+    unsigned int header_size;
+    int width;
+    int height;
+
+    unsigned short planes_qtt;
+    unsigned short bits_per_pixel;
+    unsigned int compression_type;
+
+    unsigned int image_size;
+    int horizontal_resolution;
+    int vertical_resolution;
+
+    unsigned int colors_qtt;
+    unsigned int important_colors_qtt;
+
+	fread(&header_size, SIZE_INT, 1, img_file);
+	fread(&width, SIZE_INT, 1, img_file);
+	fread(&height, SIZE_INT, 1, img_file);
+
+	fread(&planes_qtt, SIZE_SHORT, 1, img_file);
+	fread(&bits_per_pixel, SIZE_SHORT, 1, img_file);
+	fread(&compression_type, SIZE_INT, 1, img_file);
+	
+    fread(&image_size, SIZE_INT, 1, img_file);
+	fread(&horizontal_resolution, SIZE_INT, 1, img_file);
+	fread(&vertical_resolution, SIZE_INT, 1, img_file);
+
+	fread(&colors_qtt, SIZE_INT, 1, img_file);
+	fread(&important_colors_qtt, SIZE_INT, 1, img_file);
+
+    printf("Header size: %u, Width: %d, Height: %d\n", header_size, width, height);
+    printf("Color planes: %hu, Bits per pixel: %hu, Compression: %u\n", planes_qtt, bits_per_pixel, compression_type);
+    printf("Image size: %u, Horizontal resolution: %d, Vertical resolution: %d\n", image_size, horizontal_resolution, vertical_resolution);
+    printf("Colors: %u, Important colors: %u\n", colors_qtt, important_colors_qtt);
+
+    // scanning colors
+    // TODO
 
     // bye
     fclose(img_file);
