0bfffede11144fe42887f92b263b4e7c71e7af85
Mazuh <marcell-mz@hotmail.com>
2017-06-13 12:17:17 -0300

    #4 Read bmp file header

--- src/bmp.c
+++ src/bmp.c
@@ -17,9 +17,23 @@ void _bmp_read_from_file(const char *filename, int secret){
 
     // open it!
     FILE *img_file = fopen(filename, "rb");
+    printf("File: %s\n", filename);
 
-    // TODO
-    printf("%s\n", filename);
+    // scanning file header
+    //unsigned short type = 19778;
+    char type[3];
+    unsigned int size;
+    unsigned int reserved;
+    unsigned int offset;
+    
+    type[0] = fgetc(img_file);
+    type[1] = fgetc(img_file);
+    type[2] = '\0';
+    fread(&size, 4, 1, img_file);
+    fread(&reserved, 4, 1, img_file);
+    fread(&offset, 4, 1, img_file);
+
+    printf("Type: %s, Size: %u, Reserved: %u, Offset: %u\n", type, size, reserved, offset);
 
     // bye
     fclose(img_file);
