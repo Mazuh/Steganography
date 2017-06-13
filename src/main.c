64a93a7550e508258e2c6b861284a1e86d4d17d2
Mazuh <marcell-mz@hotmail.com>
2017-06-13 06:43:23 -0300

    #2 Init bmp files

--- src/main.c
+++ src/main.c
@@ -6,13 +6,30 @@
 
 #include "ppm.h"
 
-int main(){
+/* Portable PixMap */
+int test_ppm(){
     
-    printf("\n>>> TRYING TO CREATE AN IMAGE WITH SECRET...\n");
+    printf("\n>>> PPM: TRYING TO CREATE AN IMAGE WITH SECRET...\n");
     ppm_encode(PPM_DEFAULT_ASSET_FILENAME, "This project is LEGEN... Wait for it... DARY! Legendary!!!");
     
-    printf("\n>>> READING SECRET INSIDE THE CREATED IMAGE...\n");
+    printf("\n>>> PPM: READING SECRET INSIDE THE CREATED IMAGE...\n");
     printf("Discovered message: %s\n", ppm_decode(PPM_DEFAULT_ENCODED_FILENAME));
-    
+
     return 0;
+}
+
+/* Bitmap */
+int test_bmp(){
+    
+    printf("\n>>> BMP: TRYING TO CREATE AN IMAGE WITH SECRET...\n");
+    //ppm_encode(PPM_DEFAULT_ASSET_FILENAME, "This project is LEGEN... Wait for it... DARY! Legendary!!!");
+    
+    //printf("\n>>> BMP: READING SECRET INSIDE THE CREATED IMAGE...\n");
+    //printf("Discovered message: %s\n", ppm_decode(PPM_DEFAULT_ENCODED_FILENAME));
+
+    return 0;
+}
+
+int main(){
+    return test_bmp();
 }
